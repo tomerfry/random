490ec024641732c86dacd218def99b55ab8a0250
Tomer Goldschmidt <tomerfry@gmail.com>
2026-03-29 22:39:00 +0300

    Another

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Service Worker - persistence test
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', function(e) {
+  // Log all fetch requests through the SW
+});
